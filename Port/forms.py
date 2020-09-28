e40167fcb6759978173125e99916841e3cab31c6
Jokotagba Opemipo <opemipojokotagba@gmail.com>
2020-09-28 11:40:41 +0100

    upgraded django

--- Port/forms.py
+++ Port/forms.py
@@ -3,10 +3,10 @@ from django import forms
 from .models import PortfolioForm
 
 class PortfolioContactForm(forms.ModelForm):
-    Name = forms.CharField(label='Name',required=True, widget=forms.TextInput(attrs={"placeholder": "Your Name"}))
-    Email = forms.EmailField(label='Email',required=True, widget=forms.TextInput(attrs={"placeholder": "Your Email"}))
-    Subject = forms.CharField(label='Subject', required=True,widget=forms.TextInput(attrs={"placeholder": "Input your Subject"}))
-    Message = forms.CharField(label='Message',required=True,widget=forms.Textarea(attrs={"placeholder": "Your Message"})) 
+    Name = forms.CharField(label='Name',required=True, widget=forms.TextInput(attrs={"placeholder": "Name"}))
+    Email = forms.EmailField(label='Email',required=True, widget=forms.TextInput(attrs={"placeholder": "Email"}))
+    Subject = forms.CharField(label='Subject', required=True,widget=forms.TextInput(attrs={"placeholder": "Subject"}))
+    Message = forms.CharField(label='Message',required=True,widget=forms.Textarea(attrs={"placeholder": "Message"})) 
     
 
     class Meta:
--- Port/models.py
+++ Port/models.py
@@ -5,10 +5,10 @@ from django.conf import settings
 # Create your models here.
 class PortfolioForm(models.Model):
     Name = models.CharField(blank=True, null=False, max_length=40)
-    Email = models.EmailField()
+    Email = models.EmailField(blank=True, null=False, max_length=40)
     Subject = models.CharField(max_length=50)
-    Message = models.TextField()
+    Message = models.TextField(null=False, max_length=45)
 
 
     def __str__(self):
-        return self.title
+        return self.Name
